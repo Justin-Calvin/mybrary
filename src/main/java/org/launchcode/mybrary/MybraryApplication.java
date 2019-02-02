7f88d12abc27eb32d2111688518adf9b72d8ae8c
Justin K Calvin <43050992+Justin-Calvin@users.noreply.github.com>
2019-02-02 17:34:05 -0600

    Add files via upload

---
+++ src/main/java/org/launchcode/mybrary/MybraryApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.mybrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybraryApplication.class, args);
+	}
+
+}
+

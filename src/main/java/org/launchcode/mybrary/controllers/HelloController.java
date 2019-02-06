50b7f0a5c699879e070f99cc936c1fb6aafac38b
Justin Calvin <maestro3663@gmail.com>
2019-02-05 19:30:04 -0600

    8th commit redo, removed an import statement

--- src/main/java/org/launchcode/mybrary/controllers/HelloController.java
+++ src/main/java/org/launchcode/mybrary/controllers/HelloController.java
@@ -2,7 +2,6 @@ package org.launchcode.mybrary.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
